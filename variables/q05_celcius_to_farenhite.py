75d07974d50b6213818375587f3ce4ebd18804d7
Ashish Kumar <ashishlatiyan35@gmail.com>
2025-12-25 18:29:37 +0530

    Add files via upload

---
+++ variables/q05_celcius_to_farenhite.py
@@ -0,0 +1,4 @@
+celcius = int(input("enter the temperature: "))
+farenhite = (celcius * 9/5) + 32 
+print("temperature in celcius is ", celcius)
+print("temperature in farenhite ", farenhite)
---
+++ variables/q06_check_number.py
@@ -0,0 +1,8 @@
+# take input from user 
+a = float(input("enter the inteager: ")) 
+if(a>0):
+    print("It is a positive number. ")
+elif(a<0):
+    print("It is a negative number. ")
+else:
+    print("It is Zero.")

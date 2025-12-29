cad89af01b5ab34a9ca79f611171219821c939b9
Ashish Kumar <ashishlatiyan35@gmail.com>
2025-12-29 22:19:42 +0530

    add

---
+++ loops/range.py
@@ -0,0 +1,7 @@
+# range is used to geneerate a sequence of number, It is commonly used in for loop to control how many times a loop runs
+for i in range(5): #range(stop)
+    print(i)
+for i in range(2,10): # range(start,stop)
+    print(i)
+for i in range(2,101,2): # range(start,stop,step)
+    print(i)
---
+++ loops/while.py
@@ -0,0 +1,19 @@
+#x = int(input("enter the number: "))
+"""i = 1
+while i<=100:
+    print(i)
+    i+=1"""
+
+# print the number from 100 to 1 
+"""i = 100 
+while i>=1:
+    print(i)
+    i-=1
+   """
+n =int(input("enter the number: "))
+ 
+i = 1
+while i<=10:
+    print(n*i)
+    i+=1
+    

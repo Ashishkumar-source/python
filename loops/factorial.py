cfb4e8bf024e484c0529b3a9a16fcab2b062a0f0
Ashish Kumar <ashishlatiyan35@gmail.com>
2025-12-30 16:48:55 +0530

    123

---
+++ loops/factorial.py
@@ -0,0 +1,11 @@
+n =int(input("enter the number : "))
+
+fact =1
+#for i in range(1,n+1):
+ #   fact=fact*i
+#print(fact)
+i=1
+while i<=n:
+    fact*=i
+    i+=1
+print(fact)
---
+++ loops/practice5.py
@@ -0,0 +1,12 @@
+n = int(input("enter the number : "))
+
+sum = 0 
+"""for i in range(1,n+1):
+    sum+=i
+print("total sum is ",sum)
+"""
+i=1
+while i<=n:
+    sum+=i
+    i+=1
+print(sum)

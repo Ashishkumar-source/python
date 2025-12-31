3d37f88deff02654dc4dc34687c479c168af7cb7
Ashish Kumar <ashishlatiyan35@gmail.com>
2025-12-31 23:04:27 +0530

    Add files via upload

---
+++ Practice/q11_string.py
@@ -0,0 +1,17 @@
+text = input("enter the string : ")
+
+# print length of string 
+length = (len(text))
+print(length)
+
+# print the first character 
+first_character =(text[0])
+print("First character is ", first_character)
+
+# print last character of string 
+last_character = (text[-1])
+print("Last character is ",last_character)
+
+# print the string in uppercase 
+uppercase = (text.upper())
+print("string in uppercase is  ", uppercase)

656c6477709aff41a0ed69ce5a9957200b4271f5
Ashish Kumar <ashishlatiyan35@gmail.com>
2025-12-31 20:39:12 +0530

    add

---
+++ function&recursion/practice2.py
@@ -0,0 +1,9 @@
+cities =["noida","delhi","mumbai","chhenai","muzaffarngar","shamli"]
+heroes =["sunny deol","ironman","superman"]
+
+def print_list(list):
+    for item in list:
+        print(item,end=" ")
+
+print_list(heroes)
+print_list(cities)

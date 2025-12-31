37df8d0c36db501393f5075529e67e1ddb032544
Ashish Kumar <ashishlatiyan35@gmail.com>
2025-12-31 20:38:25 +0530

    Add files via upload

---
+++ function&recursion/INR.py
@@ -0,0 +1,9 @@
+def converter(usd_val):
+    inr_val = usd_val*83
+    print(usd_val,"USD = ",inr_val,"INR ")
+
+converter(1)
+converter(100)
+converter(73) 
+a= 23456789000000044444444444444444444444444444
+print(a)
---
+++ function&recursion/input.py
@@ -0,0 +1,9 @@
+def number(a):
+    if a%2==0:
+        print("even")
+    else:
+        print("Odd")
+
+
+number(2)
+number(3)
